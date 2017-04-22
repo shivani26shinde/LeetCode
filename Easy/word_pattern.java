6d37a2f193e7cb5a03b250ab56a98464105f2d8c
shivani26shinde <shindeshivani26@gmail.com>
2017-04-21 23:46:15 -0500

    Work Pattern

---
+++ Easy/word_pattern.java
@@ -0,0 +1,31 @@
+# Word Pattern
+
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        if(pattern == null && str == null){
+            return true;
+        }
+        if(pattern == null && str != null){
+            return false;
+        }
+        if(pattern != null && str == null){
+            return false;
+        }
+        String[] spt = str.split("\\s+");
+        if(pattern.length() != spt.length){
+            return false;
+        }
+        HashMap<Character,String> map = new HashMap<>();
+        for(int i=0;i<pattern.length();i++){
+            if(map.containsKey(pattern.charAt(i))){
+                if(map.get(pattern.charAt(i)).equals(spt[i])){
+                    return false;
+                }
+            }
+            else{
+                map.put(pattern.charAt(i), spt[i]);
+            }
+        }
+        return true;
+    }
+}
